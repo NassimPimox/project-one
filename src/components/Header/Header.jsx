2099d8588606c8c89eb07a770053a0c122296eee
ASUS <ASUS@DESKTOP-CBOJ3OK>
2024-01-05 15:10:24 +0100

    new

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="container">
+      <p className="heading">A <span>Resume</span> that stands out!</p>
+      <p className="heading">Make your own resume. <span>It's free</span></p>
+    </div>
+  );
+}
+
+export default Header;
